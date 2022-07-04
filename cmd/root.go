c9f5a767dd2bb46b4ddc4f0a58eb7e4e0440f49e
Simeon2001 <jesusanyasimeon@gmail.com>
2022-07-04 12:51:40 +0100

    elp-cli codes

---
+++ cmd/root.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "elp-cli",
+	Short: "Terminal Commands Search Engine",
+	Long: "elp-cli is a terminal commands search engine that help developer to search for commands base on what the command do and language or framework been used. e.g you can do (elp search django commands to runserver)",
+	
+	// Run: func(cmd *cobra.Command, args []string) { },'
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.elp-cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
